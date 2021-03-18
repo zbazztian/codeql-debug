36285741d6797d6784fd142d57d669b410b4f51b
Sebastian Bauersfeld <sebastian@semmle.com>
2021-03-18 12:37:39 +0100

    update

--- javascript-debug-pack/generate.sh
+++ javascript-debug-pack/generate.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+if test -z "$1"; then
+  echo 'usage: generate.sh /root/of/codeql-repo/checkout'
+  exit 1
+fi
+
 rm -rf source-and-sink-counts sources-and-sinks
-find ~/dev/src/ql/javascript/ql/src/Security/ -name "*.ql" -type f -print0 \
+find "${1}/javascript/ql/src/Security/" -name "*.ql" -type f -print0 \
   | xargs -0 -I {} python3 ../query-generator/generate 'javascript' '{}'
